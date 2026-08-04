79fd933e286b80e201ea05f87aac107759a3424f
adeni9294 <adeni9294@gmail.com>
2026-08-04 11:20:10 +0700

    Update page.tsx
    update

--- src/app/scan/page.tsx
+++ src/app/scan/page.tsx
@@ -49,7 +49,7 @@ export default function ScanPage() {
         }
       } catch (err: any) {
         console.error('Gagal mengambil data toko:', err.message);
-      } fontly {
+      } finally {
         setIsLoadingMeters(false);
       }
     };
